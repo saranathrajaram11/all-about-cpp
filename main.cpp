e314344b5c9e055f6708b97dd64100b7cfff0d53
saranath Rajaram <104021023+saranathrajaram11@users.noreply.github.com>
2025-02-05 19:23:03 +0530

    chess_board.cpp
    here i modified code for update the board for chess engine

--- main.cpp
+++ main.cpp
@@ -1,40 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class chess{
+  public:
+  enum pieces{
+      Wpawn=1,Wknight=2,Wbishop=3,Wrook=4,Wqueen=5,Wking=6           ,empty=0,Bpawn=-1,Bknight=-2,Bbishop=-3,Brook=-4,Bqueen=-5,Bking=-6};
+  int row=8,col=8,i,j;
+  vector<vector<char>> board;
+  chess()
+  {
+      board.resize(row, vector<char>(col,'.'));
+      assign();
+  }
+    void print_board()
+    {
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                board[i][j]='.';
+            cout<<board[i][j];
+            }
+            cout<<endl;
+        }
+    }
+    
+    void assign()
+    {
+        board[0][0]=Bknight;
+        board[0][1]=Bknight;
+        board[0][2]=Bknight;
+        board[0][3]=Bknight;
+        board[0][4]=Bknight;
+        for(j=0;j<row;j++)
+        {
+          board[1][j]=Bpawn;
+          board[6][j]=Wpawn;
+        }
+    };
+};
+
+
 int main()
 {
-    cout<< "The chess board Note : white peice marked in caps and black in smallcase";
-    int s=8;
-    char board[s][s];
-    for(int i=0;i<s;i++)
-    {
-        for(int j=0;j<s;j++)
-        {
-            board[i][j]='.';
-       // cout<<board[i][j];
-        }
-        cout<<endl;
-    }
-     for(int i=0;i<s;i++)
-    {
-        for(int j=0;j<s;j++)
-        {
-            if ((i==0 && j==0)|| (i==0 && j==7)){board [i][j]='R';}
-            if((i==7 && j==0)||(i==7 && j==7)){board[i][j]='r';}
-            if((i==0 && j==1)||(i==0 && j==6)){board[i][j]='N';}
-            if((i==7 && j==1)||(i==7 && j==6)){board[i][j]='n';}
-            if ((i==0 && j==4)){board [i][j]='K';}
-            if((i==7 && j==4)){board[i][j]='k';}
-            if (i==0 && j==3){board[i][j]='Q';}
-            if (i==7 && j==3){board[i][j]='q';}
-            if ((i==0 && j==2)|| (i==0 && j==5)){board[i][j]='B';}
-            if ((i==7 && j==2)||(i==7 && j==5)){board[i][j]='b';}
-            board[1][j]='P';
-            board[6][j]='p';
-        cout<<board[i][j];
-        }
-        cout<<endl;
-    }
-    return 0;
-
+    chess c;
+    c.print_board();
+    c.assign();
 }
